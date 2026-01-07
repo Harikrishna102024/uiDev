b6eab73f57c6a1c99120496ab039caba7ca44c34
Harikrishna102024 <tharikrishna.career@gmail.com>
2026-01-07 13:47:06 +0530

    loading and layout using grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'uiDev';
+  loading = true;
+
+  @ViewChild('menu', { static: true }) data!: ElementRef;
+
+  toggle() {
+    this.data.nativeElement.classList.toggle("open");
+  }
+
 }
